f5391fd34ca6db037336d0d014e0cbbf1480b77f
ENVER <envercanbickin@gmail.com>
2025-12-23 20:48:24 +0300

    test6

--- api.js
+++ api.js
@@ -1,8 +1,14 @@
 fetchTasks() {
-    return new Promise(()  => {
-        console.log();
+    return new Promise((resolve)  => {
+        console.log("Connecting...");
 
         setTimeout(() => {
+            const rawData = [
+            { id: 1, title: "Study JavaScript", completed: false },
+            { id: 2, title: "Practice DOM", completed: true },
+            { id: 3, title: "Read Async Patterns", completed: false }
+            ];
+            resolve(rawData);
 
         },1500);
 
