a7138b2f743d8af88d374357d46b8339c6d8b130
ENVER <envercanbickin@gmail.com>
2025-12-23 18:20:59 +0300

    test2

---
+++ taskManager.js
@@ -0,0 +1,19 @@
+class Task{
+
+    constructor(id,title,completed) {
+        Object.defineProperty(this.id,{
+            writeable: false,
+            configurable: false
+        })
+        title = this.title;
+        completed = false;
+    }
+
+
+    toggle() {
+
+        return new Task(this.id,this.title,!this.completed);
+    }
+}
+
+class TaskManager
