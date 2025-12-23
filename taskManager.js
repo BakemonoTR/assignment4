58ca3a4636894a973acf5b76a687446b2d523725
ENVER <envercanbickin@gmail.com>
2025-12-23 19:42:54 +0300

    test4

--- taskManager.js
+++ taskManager.js
@@ -2,8 +2,8 @@ class Task{
 
     constructor(id,title,completed) {
         Object.defineProperty(this.id,{
+            value: id,
             writeable: false,
-            configurable: false
         })
         title = this.title;
         completed = false;
@@ -16,3 +16,43 @@ class Task{
     }
 }
 
+class TaskManager{
+
+    constructor(tasks) {
+        tasks = this.tasks;
+
+    }
+
+
+    setTasks(tasks) {
+            this.tasks = [...tasks];
+    }
+
+
+    addTask(task) {
+        this.tasks= [...this.tasks,task];
+               
+
+    }
+
+    removeTask(taskID) {
+        const newList = this.tasks.filter(task => task.id != taskID);
+        
+        this.tasks = newList;
+    }
+
+    toggleTask(taskID) {
+        const newList = this.tasks.map(task => {
+            if (task.id == taskID) {
+                return task.toggle();
+            }
+            else {
+                return task;
+
+            }
+
+        });
+        this.tasks = newList;
+
+    }
+}
